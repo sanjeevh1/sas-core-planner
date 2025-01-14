f803790210ff4318adae55b52377a3c8846d48c8
sanjeevh1 <sanjeevharikrishnan@gmail.com>
2025-01-14 11:19:04 -0800

    Added http calls

--- hello.py
+++ hello.py
@@ -1,7 +1,21 @@
 from flask import Flask
+import aiohttp
+import asyncio
 
 app = Flask(__name__)
 
+BASE_URL = "https://sasundergrad.rutgers.edu/degree-requirements/core/"
+PATHS = [
+    "cognitive-skills-and-processes-quantitative-and-formal-reasoning", 
+    "contemporary-challenges", 
+    "cognitive-skills-and-processes-writing-and-communication", 
+    "areas-of-inquiry-arts-and-humanities", 
+    "areas-of-inquiry-social-and-historical-analysis"
+]
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+async def hello_world():
+    async with aiohttp.ClientSession() as session:
+        tasks = [session.get(BASE_URL + path) for path in PATHS]
+        responses = asyncio.gather(*tasks)
+    return responses
