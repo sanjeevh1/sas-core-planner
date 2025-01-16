48de7a9d00f4bdb4c935439f5c3068c7d87cf138
sanjeevh1 <sanjeevharikrishnan@gmail.com>
2025-01-15 18:39:25 -0800

    Added filtering by core code

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const body = document.querySelector("body")
+const coreSelect = document.getElementById("core-select");
+const courseList = document.getElementById("course-list");
+let courses;
 async function fetchCourses() {
     const response = await fetch("https://sas-core-planner.onrender.com");
-    const courses = await response.json();
+    console.log("fetched courses");
+    courses = await response.json();
+    console.log("converted to json");
     console.log(courses);
     courses.forEach(course => {
-        body.innerHTML += `
+        courseList.innerHTML += `
             <div>
                 <p>${course.number}</p>
                 <p>${course.name}</p>
@@ -14,5 +19,25 @@ async function fetchCourses() {
         `; 
     });
 }
-fetchCourses();
+async function initialize() {
+    await fetchCourses();
+    coreSelect.addEventListener("change", () => {
+        const selectedCore = coreSelect.value;
+        courseList.innerHTML = "";
+        const selectedCourses = courses.filter(course => course.core_codes.includes(selectedCore));
+        selectedCourses.forEach(course => {
+            courseList.innerHTML += `
+                <div id="${course.number}">
+                    <p>${course.number}</p>
+                    <p>${course.name}</p>
+                    <p>${course.credits}</p>
+                    <p>${course.core_codes}</p>
+                </div>
+            `;
+        });
+    });
+}
+
+initialize();
+
  
