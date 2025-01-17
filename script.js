04d61c714cff8b43fb5bc8b184fac44a3dab097e
sanjeevh1 <sanjeevharikrishnan@gmail.com>
2025-01-16 16:50:34 -0800

    Added button disabling, loading all HTML at beginning

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const body = document.querySelector("body")
 const coreSelect = document.getElementById("core-select");
 const courseList = document.getElementById("course-list");
 let courses;
+const coursesTaken = [];
 async function fetchCourses() {
     const response = await fetch("https://sas-core-planner.onrender.com");
     console.log("fetched courses");
@@ -10,31 +11,32 @@ async function fetchCourses() {
     console.log(courses);
     courses.forEach(course => {
         courseList.innerHTML += `
-            <div>
+            <div id="${course.number}-div">
                 <p>${course.number}</p>
                 <p>${course.name}</p>
                 <p>${course.credits}</p>
                 <p>${course.core_codes}</p>
+                <button id="${course.number}-btn">Add</button>
             </div>
         `; 
+    });
+    courses.forEach(course => {
+        const addButton = document.getElementById(`${course.number}-btn`);
+        addButton.addEventListener("click", () => {
+            coursesTaken.push(course);
+            console.log(coursesTaken);
+            addButton.disabled = true;
+        });
     });
 }
 async function initialize() {
     await fetchCourses();
     coreSelect.addEventListener("change", () => {
         const selectedCore = coreSelect.value;
-        courseList.innerHTML = "";
-        const selectedCourses = courses.filter(course => course.core_codes.includes(selectedCore));
-        selectedCourses.forEach(course => {
-            courseList.innerHTML += `
-                <div id="${course.number}">
-                    <p>${course.number}</p>
-                    <p>${course.name}</p>
-                    <p>${course.credits}</p>
-                    <p>${course.core_codes}</p>
-                </div>
-            `;
-        });
+        courses.forEach(course => {
+            const courseDiv = document.getElementById(`${course.number}-div`);
+            courseDiv.style.display = course.core_codes.includes(selectedCore) ? "block" : "none";
+        })
     });
 }
 
