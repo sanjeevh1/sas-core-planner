9846c6c64eacec091db51f5e5da5043a80c4025b
sanjeevh1 <sanjeevharikrishnan@gmail.com>
2025-01-19 19:46:48 -0800

    Removed checkboxes

--- script.js
+++ script.js
@@ -33,39 +33,6 @@ let ahCodesTaken = 0;
 const ahCodes = document.getElementById("AH-codes");
 const MAX_CORES = 2;
 
-function isValid(course) {
-    const checkedCores = getCheckedCores(course);
-    if (checkedCores.length === 0) {
-        alert("Please select a core code");
-        return false;
-    }
-    if (checkedCores.length > MAX_CORES) {
-        alert(`You can only select ${MAX_CORES} core codes`);
-        return false;
-    }
-    if (checkedCores.includes("CCD") && checkedCores.includes("CCO")) {
-        alert("You cannot take CCD and CCO together");
-        return false;
-    }
-    if (checkedCores.includes("WCR") && checkedCores.includes("WCD")) {
-        alert("You cannot take WCR and WCD together");
-        return false;
-    }
-    if (checkedCores.includes("QQ") && checkedCores.includes("QR")) {
-        alert("You cannot take QQ and QR together");
-        return false;
-    }
-    if (checkedCores.includes("HST") && checkedCores.includes("SCL")) {
-        alert("You cannot take HST and SCL together");
-        return false;
-    }
-    if (checkedCores.length == 2 && checkedCores[0].includes("AH") && checkedCores[1].includes("AH")) {
-        alert("You cannot take two AH codes together");
-        return false;
-    }
-    return true;
-}
-
 function addCourse(course) {
     const courseTable = document.getElementById("course-table");
     const row = courseTable.insertRow(-1);
@@ -95,20 +62,6 @@ function addCourse(course) {
     row.insertCell(-1);
 }
 
-function getCheckedCores(course) {
-    const codesDiv = document.getElementById(`${course.number}-codes`);
-    const checkboxes = codesDiv.querySelectorAll("input");
-    const checkedCores = [];
-    checkboxes.forEach(checkbox => {
-        if (checkbox.checked) {
-            checkedCores.push(checkbox.value);
-        }
-    });
-    return checkedCores;
-}
-
-
-
 async function fetchCourses() {
     const response = await fetch("http://127.0.0.1:5000");
     console.log("fetched courses");
@@ -120,28 +73,16 @@ async function fetchCourses() {
                 <p>${course.number}</p>
                 <p>${course.name}</p>
                 <p>${course.credits}</p>
-                <div id="${course.number}-codes"></div>
+                <p>${course.core_codes.join(", ")}</p>
                 <button id="${course.number}-btn">Add</button>
             </div>
         `;
-        const codesDiv = document.getElementById(`${course.number}-codes`);
-        course.core_codes.forEach(core => {
-            codesDiv.innerHTML += `
-                <input type="checkbox" value="${core}"/>
-                <label>${core}</label>
-            `;
-        }); 
     });
     courses.forEach(course => {
         const addButton = document.getElementById(`${course.number}-btn`);
         addButton.addEventListener("click", () => {
-            if(isValid(course)) {
-                addCourse(course);
-                addButton.disabled = true;
-                console.log("added course");
-            } else {
-                console.log("invalid course");
-            }
+            addCourse(course);
+            addButton.disabled = true;
         });
     });
 }
